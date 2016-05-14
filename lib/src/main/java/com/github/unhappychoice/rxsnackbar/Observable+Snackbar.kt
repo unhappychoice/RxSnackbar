e8ef598febab62e9133ac4ee25a077064c2f594e
Yuji Ueki <unhappychoice@gmail.com>
2016-05-14 13:40:00 +0900

    first commit

---
+++ lib/src/main/java/com/github/unhappychoice/rxsnackbar/Observable+Snackbar.kt
@@ -0,0 +1,29 @@
+package io.one_team.extension
+
+import android.support.design.widget.Snackbar
+import android.view.View
+import rx.Observable
+
+fun <T> Observable<T>.withNextSnackBar(view: View?, message: String? = null): Observable<T> {
+    return this.doOnNext { data ->
+        view?.let {
+            Snackbar.make(it, message ?: data.toString(), Snackbar.LENGTH_SHORT).show()
+        }
+    }
+}
+
+fun <T> Observable<T>.withErrorSnackBar(view: View?, message: String? = null): Observable<T> {
+    return this.doOnError { e ->
+        view?.let {
+            Snackbar.make(it, message ?: e.message!!, Snackbar.LENGTH_SHORT).show()
+        }
+    }
+}
+
+fun <T> Observable<T>.withCompletedSnackBar(view: View?, message: String): Observable<T> {
+    return this.doOnCompleted {
+        view?.let {
+            Snackbar.make(it, message, Snackbar.LENGTH_SHORT).show()
+        }
+    }
+}
